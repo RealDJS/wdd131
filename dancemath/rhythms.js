c8893e8876fcfd7efbc5fc3e6c76380f11cf219a
RealDJS <dj@skoys.com>
2025-12-11 20:12:55 -0700

    adding styling and starting rhythms json

---
+++ dancemath/rhythms.js
@@ -0,0 +1,23 @@
+const rhythms = [
+   {
+      name: "single",
+      img: "images/single.png",
+      description:
+         "One weight change in 2 beats. (You step on beat 1, and wait on beat 2)",
+      weightChanges: 1,
+   },
+   {
+      name: "double",
+      img: "images/double.png",
+      description:
+         "Two weight changes in 2 beats. (You step on beat 1, and step on beat 2)",
+      weightChanges: 2,
+   },
+   {
+      name: "triple",
+      img: "images/triple.png",
+      description:
+         "Three weight changes in 2 beats. (You step on 1, step in between, and step on 2)",
+      weightChanges: 3,
+   },
+];
